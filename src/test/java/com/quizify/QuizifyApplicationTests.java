210e92f2bef29fed01d3c0fae0c60768baae8319
caotrang71 <caotrang7141@gmail.com>
2024-06-04 14:14:58 +0700

    pom.xml and application.properties updated

--- src/test/java/com/quizify/QuizifyApplicationTests.java
+++ src/test/java/com/quizify/QuizifyApplicationTests.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.quizify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class QuizifyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
