db2c9ba0c81858eb260b8e603e172dd47bee7417
DieuLinhhe176833 <linhvdhe176833@fpt.edu.vn>
2024-06-10 10:05:02 +0700

    Add funtion Change password

--- src/main/java/com/example/demo/Controller/UsersController.java
+++ src/main/java/com/example/demo/Controller/UsersController.java
@@ -2,11 +2,14 @@ package com.example.demo.Controller;
 
 import com.example.demo.Entity.Users;
 import com.example.demo.Repository.UsersRepository;
+import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -14,6 +17,8 @@ import java.util.Optional;
 public class UsersController {
     @Autowired
     private UsersRepository usersRepository;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/profile/{id}")
     public String profile(@PathVariable int id, Model model) {
@@ -21,4 +26,28 @@ public class UsersController {
         model.addAttribute("user", user);
         return "profile";
     }
+
+    @GetMapping("/change_pass/{username}")
+    public String showChangePasswordForm(@PathVariable String username, Model model) {
+        Users user = usersRepository.findByUsername(username);
+        model.addAttribute("user", user);
+        return "ChangePassword";
+    }
+
+    @PostMapping("/changepassword/{username}")
+    public String changePassword(@PathVariable String username,
+                                 @RequestParam("oldPassword") String oldPassword,
+                                 @RequestParam("newPassword") String newPassword,
+                                 @RequestParam("confirmPassword") String ConfirmPassword,
+                                 Model model) {
+        if (userService.changePassword(username, oldPassword, newPassword, ConfirmPassword)) {
+            model.addAttribute("mess", "Change password successfully!");
+
+            return "redirect:/profile/1"; // Chuyển hướng đến trang profile nếu thay đổi thành công
+        } else {
+            model.addAttribute("mess", "Change password failed!");
+            return "redirect:/change_pass?error"; // Chuyển hướng lại trang thay đổi mật khẩu với thông báo lỗi
+        }
+    }
+
 }
---
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.demo.Service;
+
+import com.example.demo.Entity.Users;
+import com.example.demo.Repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UsersRepository UsersRepository;
+
+    public boolean changePassword(String username,String oldPassword, String newPassword, String comfirmpass) {
+        // Kiểm tra mật khẩu cũ
+        Users user = UsersRepository.findByUsername(username);
+        if (user == null || !user.getPassword().equals(oldPassword)) {
+            return false; // Mật khẩu cũ không đúng
+        }
+
+        // Kiểm tra mật khẩu mới và mật khẩu xác nhận
+        if (!newPassword.equals(comfirmpass)) {
+            return false; // Mật khẩu mới và mật khẩu xác nhận không khớp
+        }
+
+        // Cập nhật mật khẩu mới trong cơ sở dữ liệu
+        user.setPassword(newPassword);
+        UsersRepository.save(user);
+        return true;
+    }
+}
